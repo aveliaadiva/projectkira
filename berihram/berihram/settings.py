8fae124a2b3f363d6f62b2c0a9d6fd17aac9bd0a
aveliaa <avelia.diva@ui.ac.id>
2023-01-16 15:15:32 +0700

    page-1-base

--- berihram/berihram/settings.py
+++ berihram/berihram/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
