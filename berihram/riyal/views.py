52badde2f2bfee6819b6936aa711cd3307beb954
aveliaa <avelia.diva@ui.ac.id>
2023-01-16 16:56:19 +0700

    riyal basic feature

--- berihram/riyal/views.py
+++ berihram/riyal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Video
+
 def index(request):
-	return render(request, 'page-riyal.html')
+    videos = Video.objects.all()
+    return render(request, 'page-riyal.html',context={'videos':videos})
