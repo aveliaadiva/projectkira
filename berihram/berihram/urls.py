8af154dbd4ccf354148686243865dc7e0e79616b
aveliaa <avelia.diva@ui.ac.id>
2023-01-17 13:17:58 +0700

    page 2 completed

--- berihram/berihram/urls.py
+++ berihram/berihram/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main', include('main.urls')),
+    path('', include('main.urls')),
     path('riyal', include('riyal.urls')),
 ]
