553b70c83603260df511152c17a551b48fc56b14
aveliaa <avelia.diva@ui.ac.id>
2023-01-17 15:35:37 +0700

    SWIFT SAME DAY done

--- berihram/berihram/settings.py
+++ berihram/berihram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'riyal',
+    'swiftsd',
     'embed_video',
 ]
 
--- berihram/berihram/urls.py
+++ berihram/berihram/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('riyal', include('riyal.urls')),
+    path('swift-same-day', include('swiftsd.urls')),
 ]
