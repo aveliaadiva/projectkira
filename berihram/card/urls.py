772e3aadd212558b213f101019f32ccf7cbf6278
aveliaa <avelia.diva@ui.ac.id>
2023-01-20 11:35:51 +0700

    master done

--- berihram/card/urls.py
+++ berihram/card/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from .views import visa
+from .views import visa,master
 
 urlpatterns = [
     path('/visa', visa, name='visa'),
+    path('/master',master,name='master')
 ]
--- berihram/card/views.py
+++ berihram/card/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def visa(request):
 	return render(request, 'visa-page.html')
+
+def master(request):
+	return render(request, 'master-page.html')
