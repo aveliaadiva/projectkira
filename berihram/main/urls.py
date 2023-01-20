efa9a149ef6a3a3012a71292a094ee4ac09f1fe2
aveliaa <avelia.diva@ui.ac.id>
2023-01-20 14:47:30 +0700

    rekanan done

--- berihram/main/urls.py
+++ berihram/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import index,syarat
+from .views import index,syarat,rekan
 
 urlpatterns = [
     path('', index, name='index'),
     path('syarat-haji', syarat, name='syarat'),
+    path('rekan', rekan, name='rekan'),
 ]
--- berihram/main/views.py
+++ berihram/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'page.html')
+	return render(request, 'home-page.html')
 
 def syarat(request):
 	return render(request, 'syarat-page.html')
+
+def rekan(request):
+	return render(request, 'rekan-page.html')
