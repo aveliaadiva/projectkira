b13149908991c4689bc03d35ea54f3bc272a10fd
aveliaa <avelia.diva@ui.ac.id>
2023-01-28 20:36:10 +0700

    fixed model at url

--- riyal/models.py
+++ riyal/models.py
@@ -5,7 +5,7 @@ from embed_video.fields import EmbedVideoField
 class Video(models.Model):
     title = models.CharField(max_length=100)
     added = models.DateTimeField(auto_now_add=True)
-    url = EmbedVideoField()
+    url = models.CharField(max_length=200)
 
     def __str__(self):
         return str(self.title)
